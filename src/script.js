9699048404f6c5c71cf1c4d9e6605f4a994b2257
Arm4nd7 <armando.estevezt@gmail.com>
2025-05-23 10:24:47 -0500

    Add a search container and button action

--- src/script.js
+++ src/script.js
@@ -1,34 +1,54 @@
 const containerDataApi = document.getElementById("container-data-api");
 const testButton = document.querySelector(".test-button");
-const testInput = document.querySelector("input");
+let input = document.querySelector(".test-search");
+let dataApiArray = [];
+let dataInputArray = [];
 
-
-let dataApiArray = []
 
 testButton.addEventListener("click", () => {
-
-    let input = document.querySelector(".test-search");
-    let p = document.querySelector(".test-element");
-    p.textContent = input.value;
+    // dataInputArray.push(input.value);
+    // console.log(dataInputArray);
+    if(dataApiArray.length < 2){
+        filterSearch();
+    }else if (dataApiArray.length > 1) {
+        let dataRemove = dataApiArray.pop();
+        
+    }
 });
+
 
 fetch("https://restcountries.com/v3.1/all")
     .then((response) => {
         return response.json();
-    }).then(data => {
-        showData(data);
+    }).then(countries => {
+        dataApiArray = countries;
+        showData(dataApiArray)
     })
 
-
-    
 function showData(countries) {
-    for (let country = 0; country < countries.length; country++) {
+    countries.forEach((country) => {
         const div = document.createElement("div");
+        const p = document.createElement("p");
+        const pContinent = document.createElement("p");
         div.classList.add("card");
-        div.innerHTML = `<strong>${countries[country].continents}</strong>`;
+        pContinent.innerHTML = `<strong>${country.continents}</strong>`;
+        p.innerHTML =`<strong>${country.name.common}</strong>`;
+        div.appendChild(pContinent);
+        div.appendChild(p);
         containerDataApi.appendChild(div);
-    }
+    })
 }
 
+function filterSearch() {
+    containerDataApi.innerHTML = "";
+    let pais = dataApiArray.filter(country => country.name.common.includes(input.value));
+    const storeWord = document.createElement("div");
+    const pSotoreWord = document.createElement("p");
+    storeWord.classList.add("newCard");
+    pSotoreWord.innerHTML = `<strong>${pais[0].name.common}</strong>`;
+    storeWord.appendChild(pSotoreWord);
+    containerDataApi.appendChild(storeWord);
+    console.log(pais);
+}
 
 
