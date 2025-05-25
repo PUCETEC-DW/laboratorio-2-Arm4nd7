be4b9492d59af0823f89b179b195513281c1db2d
Arm4nd7 <armando.estevezt@gmail.com>
2025-05-24 23:20:06 -0500

    add dynamic filter for country names based on user input

--- src/script.js
+++ src/script.js
@@ -1,69 +1,72 @@
 const containerDataApi = document.getElementById("container-data-api");
-const testButton = document.querySelector(".test-button");
-let input = document.querySelector(".test-search");
+const searchButton = document.querySelector(".test-button");
+let searchInput = document.querySelector(".test-search");
 let dataApiArray = [];
 let dataInputArray = [];
 
-testButton.addEventListener("click", () => {
-    const valorInput = input.value.trim();
-    if (valorInput !== "") {
-        dataInputArray = [valorInput]; // solo guarda 1 valor
-        filterSearch(valorInput);
-        console.log(dataInputArray);
+function createCardCountry(country) {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    const pName = document.createElement("p");
+    const pRegion = document.createElement("p");
+    let imgFlag = document.createElement("div");
+    const pPopulation = document.createElement("p");
+    const pContinents = document.createElement("p");
+    pName.innerHTML = `<strong>${country.name.official}</strong>`;
+    pRegion.innerHTML = `<strong>${country.region}</strong>`;
+    imgFlag.innerHTML = `<img src="${country.flags.png}" alt="Bandera de ${country.name.common}">`;
+    pPopulation.innerHTML = `<strong>${country.population}</strong>`;
+    pContinents.innerHTML = `<strong>${country.continents}</strong>`;
+    div.appendChild(pName);
+    div.appendChild(pRegion);
+    div.appendChild(pPopulation);
+    div.appendChild(pContinents);
+    div.appendChild(imgFlag);
+    return div;
+}
+
+function showCardCountry(countries) {
+    containerDataApi.innerHTML = "";
+    if (countries.length === 0) {
+        window.alert("NO RESULTS, TRY AGAIN");
+        containerDataApi.innerHTML = `<p>NO RESULTS, TRY AGAIN</p>`;
+        return;
     }
-});
+    countries.forEach((country) => {
+        containerDataApi.appendChild(createCardCountry(country));
+    });
+}
+
+function searchAndFilterName(texto) {
+    const pais = dataApiArray.filter(country =>
+        country.name.common.toLowerCase().includes(texto.toLowerCase())
+    );
+    return pais;
+}
 
 fetch("https://restcountries.com/v3.1/all")
     .then((response) => {
         return response.json();
     }).then(countries => {
         dataApiArray = countries;
-        mainShowData();
+        showCardCountry(dataApiArray);
     })
 
-function mainShowData() {
-    dataApiArray.forEach((country) => {
-        const div = document.createElement("div");
-        const pNmae = document.createElement("p");
-        const pRegion = document.createElement("p");
-        let imgFlag = document.createElement("div");
-        const pPopulation = document.createElement("p");
-        const pContinents = document.createElement("p");
-        div.classList.add("card");
-        pNmae.innerHTML = `<strong>${country.name.official}</strong>`;
-        pRegion.innerHTML = `<strong>${country.region}</strong>`;
-        imgFlag.innerHTML = `<img src="${country.flags.png}" alt="Bandera de ${country.name.common}">`;
-        pPopulation.innerHTML = `<strong>${country.population}</strong>`;
-        pContinents.innerHTML = `<strong>${country.continents}</strong>`;
-
-        div.appendChild(pNmae);
-        div.appendChild(pRegion);
-        div.appendChild(imgFlag);
-        div.appendChild(pPopulation);
-        div.appendChild(pContinents);
-
-        containerDataApi.appendChild(div);
-    })
-}
-
-function filterSearch(texto) {
-    containerDataApi.innerHTML = "";
-    const pais = dataApiArray.filter(country => 
-        country.name.common.toLowerCase().includes(texto.toLowerCase())
-    );
-    for (let i = 0; i < pais.length; i++) {
-        const storeWord = document.createElement("div");
-        const pSotoreWord = document.createElement("p");
-        // const flag = document.createElement("div");
-
-        storeWord.classList.add("newCard");
-        console.log(pais[i]);
-        pSotoreWord.innerHTML = `<strong>${pais[i].capital}</strong>`;
-        // flag.innerHTML = `<img src="${pais[i].flags.png}">`;
-        storeWord.appendChild(pSotoreWord);
-        // storeWord.appendChild(flag);
-        containerDataApi.appendChild(storeWord);
+searchButton.addEventListener("click", () => {
+    const valorInput = searchInput.value.trim();
+    if (valorInput === "") {
+        window.alert("Empty values to search");
+        showCardCountry(dataApiArray)
+    } else {
+        showCardCountry(searchAndFilterName(valorInput))
     }
-}
+});
 
-
+searchInput.addEventListener("input", () => {
+    const valorInput = searchInput.value.trim();
+    if (valorInput === "") {
+        showCardCountry(dataApiArray)
+    } else {
+        showCardCountry(searchAndFilterName(valorInput))
+    }
+});
